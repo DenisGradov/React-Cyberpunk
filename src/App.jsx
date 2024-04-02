903dc8c7a7ba87600eef83f4898717cc0b34c33f
MSPHelperSamp <155314413+MSPHelperSamp@users.noreply.github.com>
2024-04-02 20:57:27 +0200

    Добавлен адаптивный блок с заполнением формы

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 function App() {
   const PHOTO_SRC = {};
   PHOTO_SRC["logo"] = "../public/images/Logo.png";
+  PHOTO_SRC["promotion"] = "../public/images/other/promotion.svg";
   PHOTO_SRC["fb"] = "../public/images/icons/fb.png";
   PHOTO_SRC["inst"] = "../public/images/icons/inst.svg";
   PHOTO_SRC["twich"] = "../public/images/icons/twich.svg";
@@ -20,6 +21,14 @@ function App() {
     "../public/images/aboutCity/1.png",
     "../public/images/aboutCity/2.jpg",
     "../public/images/aboutCity/3.png",
+  ];
+  PHOTO_SRC["other"] = [
+    "../public/images/other/bg1.png",
+    "../public/images/other/bg2.png",
+    "../public/images/other/blue line.png",
+    "../public/images/other/yellow line.png",
+    "../public/images/other/xbox.png",
+    "../public/images/other/ps.png",
   ];
 
   const [actualHeaderPhoto, setActualHeaderPhoto] = useState(1);
@@ -124,6 +133,155 @@ function App() {
               src={PHOTO_SRC["aboutCity"][1]}
             />
           </div>
+        </div>
+        <div className={styles.blockWithForm}>
+          <img
+            className={styles.blockWithFormContent__startBg}
+            src={PHOTO_SRC["other"][0]}
+          />
+          <div className={styles.blockWithFormContent}>
+            <div className={styles.blockWithFormContentTextbox}>
+              <div className={styles.blockWithFormContentTextboxTop}>
+                <img
+                  className={styles.blockWithFormContentTextboxTop__photo}
+                  src={PHOTO_SRC["promotion"]}
+                />
+                <h2 className={styles.blockWithFormContentTextboxTop__title}>
+                  Играй и выигрывай!
+                </h2>
+              </div>
+              <h2 className={styles.blockWithFormContentTextbox__text}>
+                Играй в{" "}
+                <span className={styles.blockWithFormContentTextbox__textBlue}>
+                  Cyberpunk 2077
+                </span>{" "}
+                и получи возможность выиграть консоль{" "}
+                <span className={styles.blockWithFormContentTextbox__textBlue}>
+                  Xbox Series X
+                </span>{" "}
+                или{" "}
+                <span className={styles.blockWithFormContentTextbox__textBlue}>
+                  Sony PlayStation 5!
+                </span>{" "}
+                Заполни форму ниже и приложи скриншот о покупке игры. Итоги
+                розыгрыша будут подведены 1 февраля. Удачи! ;{")"}
+              </h2>
+            </div>
+            <div className={styles.blockWithFormContentContentbox}>
+              <div className={styles.blockWithFormContentContentboxWithForm}>
+                <input
+                  placeholder="Как тебя зовут?"
+                  className={
+                    styles.blockWithFormContentContentboxWithForm__input
+                  }
+                ></input>
+                <input
+                  placeholder="Твой e-mail"
+                  className={
+                    styles.blockWithFormContentContentboxWithForm__input
+                  }
+                ></input>
+                <div
+                  className={styles.blockWithFormContentContentboxWithFormFile}
+                >
+                  <div
+                    className={
+                      styles.blockWithFormContentContentboxWithFormFileTextbox
+                    }
+                  >
+                    <h2
+                      className={
+                        styles.blockWithFormContentContentboxWithFormFileTextbox__textMain
+                      }
+                    >
+                      Прикрепить скриншот
+                    </h2>
+                    <h2
+                      className={
+                        styles.blockWithFormContentContentboxWithFormFileTextbox__text
+                      }
+                    >
+                      .png / .jpg / .pdf
+                    </h2>
+                    <input
+                      type="file"
+                      className={
+                        styles.blockWithFormContentContentboxWithFormFile__inputFile
+                      }
+                    ></input>
+                  </div>
+                </div>
+                <button
+                  className={
+                    styles.blockWithFormContentContentboxWithForm__button
+                  }
+                >
+                  Отправить
+                </button>
+                <div
+                  className={
+                    styles.blockWithFormContentContentboxWithFormAccept
+                  }
+                >
+                  <input
+                    type="checkbox"
+                    className={
+                      styles.blockWithFormContentContentboxWithFormAccept__checkbox
+                    }
+                  ></input>
+                  <h2
+                    className={
+                      styles.blockWithFormContentContentboxWithFormAccept__text
+                    }
+                  >
+                    Согласен на обработку персональных данных
+                  </h2>
+                </div>
+              </div>
+              <div className={styles.blockWithFormContentContentboxWithPhoto}>
+                <div
+                  className={
+                    styles.blockWithFormContentContentboxWithPhotoBlock
+                  }
+                >
+                  <img
+                    className={
+                      styles.blockWithFormContentContentboxWithPhotoBlock__img
+                    }
+                    src={PHOTO_SRC["other"][3]}
+                  />
+                  <img
+                    className={
+                      styles.blockWithFormContentContentboxWithPhotoBlock__img
+                    }
+                    src={PHOTO_SRC["other"][4]}
+                  />
+                </div>
+                <div
+                  className={
+                    styles.blockWithFormContentContentboxWithPhotoBlock
+                  }
+                >
+                  <img
+                    className={
+                      styles.blockWithFormContentContentboxWithPhotoBlock__img
+                    }
+                    src={PHOTO_SRC["other"][2]}
+                  />
+                  <img
+                    className={
+                      styles.blockWithFormContentContentboxWithPhotoBlock__img
+                    }
+                    src={PHOTO_SRC["other"][5]}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            className={styles.blockWithFormContent__endBg}
+            src={PHOTO_SRC["other"][1]}
+          />
         </div>
       </div>
     </>
